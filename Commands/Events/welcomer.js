c8067bf1a075dee562133c1b38dfdaeddeac8c2c
NotFaizen <83504931+NotFaizen@users.noreply.github.com>
2021-10-02 14:59:48 +0600

    Update welcomer.js

--- Commands/Events/welcomer.js
+++ Commands/Events/welcomer.js
@@ -3,7 +3,7 @@ type: "joinCommand",
 channel: "$getServerVar[welcomeChannel]",
 code: `
 $replaceText[$replaceText[$replaceText[$getServerVar[welcomeMsg];{user.ping};<@$authorID>;-1];{user.name};$username;-1];{user.tag};$usertag;-1]
-$attachment[https://api.popcatdev.repl.co/welcomecard?background=$get[bg]&text1=$username%23$discriminator&text2=$uri[encode;$get[msg]]&text3=Member+$math[$membersCount+1]&avatar=$replaceText[$replaceText[$replaceText[$replaceText[$authorAvatar;webp;png;-1];jpg;png;-1];gif;png;-1];?size=4096;];welcome.png]
+$attachment[https://api.popcat.xyz/welcomecard?background=$get[bg]&text1=$username%23$discriminator&text2=$uri[encode;$get[msg]]&text3=Member+$math[$membersCount+1]&avatar=$replaceText[$replaceText[$replaceText[$replaceText[$authorAvatar;webp;png;-1];jpg;png;-1];gif;png;-1];?size=4096;];welcome.png]
 $onlyif[$getServerVar[welcomeChannel]!=;]
 $let[bg;$getServerVar[welcomeBg]]
 $let[msg;Welcome To $serverName]`
