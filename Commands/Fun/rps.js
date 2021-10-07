1674171ad523c17b53e4199d306bf80a79a84859
NotFaizen <83504931+NotFaizen@users.noreply.github.com>
2021-10-07 13:50:50 +0000

    added anime info command and updated rps command

--- Commands/Fun/rps.js
+++ Commands/Fun/rps.js
@@ -1,15 +1,64 @@
-module.exports = ({
- name: "rps",
- aliases: ['roshambo'],
- code: `$awaitReaction[$authorID;15s;
-{title:🧠 RPS GAME}
-{thumbnail:$userAvatar[$clientID]}
-{description:Choose your choice
-✊ | **ROCK** 
-📄 | **PAPER**
-✂ | **SCISSOR**}
-{footer:Requested By $userTag[$authorID]:$authorAvatar}
-{timestamp}
-{color:$getUserVar[embedColor]}
-;✊,📄,✂;rock,paper,scissor;You ran out of time]`
-})
+module.exports = [{
+  name: 'rockpaperscissor',
+  aliases: ['rockpaperscissor', 'rps'],
+  category: 'fun',
+  description: 'Play Rock Paper Scissor With Me!',
+  code: `
+$reactionCollector[$splitText[1];$authorID;1m;✊,📄,✂;Rock,Paper,Scissor;yes]
+$textSplit[$sendMessage[{title:Rock Paper Scissor!}
+{color:GREEN}
+{footer:RPS Game Command}
+{description:Choose Rock, Paper Or Scissor In Reaction!};yes]; ]`
+}, {
+        type: 'awaitedCommand',
+        name: 'Rock',
+        code: `$editMessage[$message[1];{title:🧠 RPS GAME} {description:$randomText[
+**$username[$clientID] ✊ Vs ✊ You**
+
+🟡 │ It's a **TIE**.;
+**$username[$clientID] 📄 Vs ✊ You**
+
+🔴 │ You **LOST**.;
+**$username[$clientID] ✂ Vs ✊ You**
+
+🟢 │You **WON**]
+
+$randomText[🎉 Well Played !;🎉 Good Game !]} {color:GREEN} {footer: RPS Game!} {timestamp:ms} {thumbnail:$userAvatar[$clientID]}
+]
+`
+}, {
+        type: 'awaitedCommand',
+        name: 'Paper',
+        code: `
+        $editMessage[$message[1];{title:🧠 RPS GAME} {description:$randomText[
+**$username[$clientID] 📄 Vs 📄 You**
+
+🟡 │ It's a **TIE**.;
+**$username[$clientID] ✂ Vs 📄 You**
+
+🔴 │ You **LOST**.;
+**$username[$clientID] ✊ Vs 📄 You**
+
+🟢 │You **WON**.]
+
+$randomText[🎉 Well Played !;🎉 Good Game !]} {color:GREEN} {footer: RPS Game!} {timestamp:ms} {thumbnail:$userAvatar[$clientID]}
+`
+}, {
+        type: 'awaitedCommand',
+        name: 'Scissor',
+        code: `
+$editMessage[$message[1];{title:🧠 RPS GAME} {description:$randomText[
+**$username[$clientID] ✂ Vs ✂ You**
+
+🟡 │ It's a **TIE**.;
+**$username[$clientID] ✊ Vs ✂ You**
+
+🔴 │ You **LOST**.;
+**$username[$clientID] 📄 Vs ✂ You**
+
+🟢 │You **WON**.]
+
+$randomText[🎉 Well Played !;🎉 Good Game !]} {color:GREEN} {footer: RPS Game!} {timestamp:ms} {thumbnail:$userAvatar[$clientID]}
+]
+`
+}]
---
+++ Commands/Misc/anime.js
@@ -0,0 +1,12 @@
+module.exports = ({
+name: "anime",
+code: `$title[$getObjectProperty[anime.title];$getObjectProperty[anime.url]]
+$thumbnail[$getObjectProperty[anime.picture]]
+$description[$getObjectProperty[anime.synopsis]]
+$addField[Trailer:;**[Youtube video]($getObjectProperty[anime.trailer])**;yes]
+$addField[Age Rating:;$replaceText[$getObjectProperty[anime.rating];R - ;];yes]
+$addField[Type:;$getObjectProperty[anime.type];yes]
+$footer[Score: $getObjectProperty[anime.score] | Favorites: $getObjectProperty[anime.favorites]]
+$color[$getUserVar[embedColor]]
+$createObject[$jsonRequest[https://husky.huskyio.repl.co/anime?key=husky&name=$uri[encode;$message]]`
+})
